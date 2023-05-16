16a975cd57e13be8ea11d87b49958bc66e76b387
Sadia Waheed <sadia.waheed312@hotmail.com>
2023-05-16 21:18:49 +0300

    react-parallax-tilt package installation and implementation

--- src/components/Logo/Logo.js
+++ src/components/Logo/Logo.js
@@ -1,10 +1,16 @@
-import React from 'react';
+import React from "react";
+import Tilt from "react-parallax-tilt";
 
 const Logo = () => {
-    return(
-        <div className='ma4 mt0'>
+  return (
+    <div className="ma4 mt0">
+      <Tilt className="Tilt br2 shadow-2">
+        <div style={{ height: "300px", backgroundColor: "darkgreen" }}>
+          <h1>React Parallax Tilt 👀</h1>
         </div>
-    );
-}
+      </Tilt>
+    </div>
+  );
+};
 
 export default Logo;
