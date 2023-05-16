55fc0c47045695047ba6eb946ef133a2ab07547b
Sadia Waheed <sadia.waheed312@hotmail.com>
2023-05-16 20:54:48 +0300

    Add Navigation component and tachyon package

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
+import Navigation from "./components/Navigation/Navigation";
 
-function App() {
-  return (
-    <div className="App">
-      
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Navigation />
+      </div>
+    );
+  }
 }
 
 export default App;
