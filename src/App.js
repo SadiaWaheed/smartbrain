6bf6339a580296a9e93e5bf4050adf65c8337d2d
Sadia Waheed <sadia.waheed312@hotmail.com>
2023-05-17 20:50:43 +0300

    Implementing Clarifai API

--- src/App.js
+++ src/App.js
@@ -1,20 +1,103 @@
 import React, { Component } from "react";
-import "./App.css";
-import ParticlesBg from"particles-bg";
+import ParticlesBg from "particles-bg";
 import Navigation from "./components/Navigation/Navigation";
 import ImageLinkForm from "./components/ImageLinkForm/ImageLinkForm";
 import Logo from "./components/Logo/Logo";
 import Rank from "./components/Rank/Rank";
+import "./App.css";
+
+const returnClarifaiRequestOptions = (imageUrl) => {
+  // Your PAT (Personal Access Token) can be found in the portal under Authentification
+  const PAT = "50aff807302844d9828e1518ccd9d882";
+  // Specify the correct user_id/app_id pairings
+  // Since you're making inferences outside your app's scope
+  const USER_ID = "txg0oa4xu0zl";
+  const APP_ID = "smart-brain";
+  // Change these to whatever model and image URL you want to use
+  //const MODEL_ID = "face-detection";
+  const IMAGE_URL = imageUrl;
+
+  const raw = JSON.stringify({
+    user_app_id: {
+      user_id: USER_ID,
+      app_id: APP_ID,
+    },
+    inputs: [
+      {
+        data: {
+          image: {
+            url: IMAGE_URL,
+          },
+        },
+      },
+    ],
+  });
+
+  const requestOptions = {
+    method: "POST",
+    headers: {
+      Accept: "application/json",
+      Authorization: "Key " + PAT,
+    },
+    body: raw,
+  };
+
+  return requestOptions;
+};
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      input: "",
+    };
+  }
+
+  onInputChange = (event) => {
+    console.log(event);
+  };
+
+  onButtonSubmit = () => {
+    this.setState({ imageUrl: this.state.input });
+
+    fetch(
+      "https://api.clarifai.com/v2/models/" + "face-detection" + "/outputs",
+      returnClarifaiRequestOptions(this.state.input)
+    )
+      .then((response) => response.json())
+      .then((response) => {
+        console.log("hi", response);
+        if (response) {
+          fetch("http://localhost:3000/image", {
+            method: "put",
+            headers: { "Content-Type": "application/json" },
+            body: JSON.stringify({
+              ID: this.state.user.id,
+            }),
+          });
+        }
+      })
+      .catch((error) => console.log("error", error));
+  };
+
   render() {
     return (
       <div className="App">
-        <ParticlesBg className="particles" color="#FFFFFF" num={300} type="cobweb" bg={true} />
+        <ParticlesBg
+          className="particles"
+          color="#FFFFFF"
+          num={300}
+          type="cobweb"
+          bg={true}
+        />
         <Navigation />
         <Logo />
         <Rank />
-        <ImageLinkForm />
+        <ImageLinkForm
+          onInputChange={this.onInputChange}
+          onButtonSubmit={this.onButtonSubmit}
+        />
+        {/* <FaceRecognition /> */}
       </div>
     );
   }
