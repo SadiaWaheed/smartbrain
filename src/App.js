817379dd682626d3b85174a77e0ecfa4380f1678
Sadia Waheed <sadia.waheed312@hotmail.com>
2023-05-17 16:06:54 +0300

    Implementing Particles-bg

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import "./App.css";
+import ParticlesBg from"particles-bg";
 import Navigation from "./components/Navigation/Navigation";
 import ImageLinkForm from "./components/ImageLinkForm/ImageLinkForm";
 import Logo from "./components/Logo/Logo";
@@ -9,6 +10,7 @@ class App extends Component {
   render() {
     return (
       <div className="App">
+        <ParticlesBg className="particles" color="#FFFFFF" num={300} type="cobweb" bg={true} />
         <Navigation />
         <Logo />
         <Rank />
