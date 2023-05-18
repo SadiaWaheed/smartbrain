cff2a3ff64558fabb4e4374c37381a414c3a2010
Sadia Waheed <sadia.waheed312@hotmail.com>
2023-05-18 17:50:46 +0300

    Implementing Face detection

--- src/App.js
+++ src/App.js
@@ -51,9 +51,31 @@ class App extends Component {
     super();
     this.state = {
       input: "",
-      imageUrl:""
+      imageUrl:"",
+      box:{}
     };
   }
+
+caluculateFaceLocation = (data) =>{
+  const clarifaiFace = data.outputs[0].data.regions[0].region_info.bounding_box;
+  const image = document.getElementById('inputimage');
+  const width = Number(image.width);
+  const height = Number(image.height);
+  console.log(width, height)
+  console.log(clarifaiFace)
+  return{
+    leftCol : clarifaiFace.left_col * width,
+    topRow : clarifaiFace.top_row * height,
+    rightCol : width -(clarifaiFace.right_col * width),
+    bottomRow : height - (clarifaiFace.bottom_row * height)
+    
+  }
+}
+
+displayFaceBox = (box) => {
+  console.log(box)
+  this.setState({box:box});
+}
 
   onInputChange = (event) => {
     this.setState({ input: event.target.value});
@@ -67,22 +89,8 @@ class App extends Component {
       returnClarifaiRequestOptions(this.state.input)
     )
       .then((response) => response.json())
-      .then((response) => {
-        if (response) {
-        console.log("hi", response.outputs[0].data.regions[0].region_info);
-         /*  fetch("http://localhost:3000/image", {
-            method: "put",
-            headers: { "Content-Type": "application/json" },
-            body: JSON.stringify({
-              id: this.state.user.id,
-            }),
-          })
-          .then(response => response.json())
-          .then(count => {
-            this.setState(Object.assign(this.state.user, {entries:count}))
-          }) */
-        }
-      })
+      .then((response) => this.displayFaceBox(this.caluculateFaceLocation(response))
+      )
       .catch((error) => console.log("error", error));
   };
 
@@ -97,13 +105,13 @@ class App extends Component {
           bg={true}
         />
         <Navigation />
-        <Logo />
+        <Logo />s
         <Rank />
         <ImageLinkForm
           onInputChange={this.onInputChange}
           onButtonSubmit={this.onButtonSubmit}
         />
-        <FaceRecognition imageUrl={this.state.imageUrl}/>
+        <FaceRecognition box={this.state.box} imageUrl={this.state.imageUrl}/>
       </div>
     );
   }
