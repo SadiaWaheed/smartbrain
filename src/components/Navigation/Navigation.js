4fc6e7ecc2a31cb12d257d7040e5d06bd1959d6d
Sadia Waheed <sadia.waheed312@hotmail.com>
2023-05-19 14:59:27 +0300

    Implementing signout

--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -1,11 +1,31 @@
 import React from "react";
 
-const Navigation = ({onRouteChange}) =>{
-    return(
-        <div style={{display:'flex', justifyContent:'flex-end'}}>
-            <p onClick={() => onRouteChange('signin')} className="f3 link dim black underline pa3 pointer">Sign Out</p>
-        </div>
-    );
-}
+const Navigation = ({ onRouteChange, isSignedIn }) => {
+  return isSignedIn ? (
+    <div style={{ display: "flex", justifyContent: "flex-end" }}>
+      <p
+        onClick={() => onRouteChange("signout")}
+        className="f3 link dim black underline pa3 pointer"
+      >
+        Sign Out
+      </p>
+    </div>
+  ) : (
+    <div style={{ display: "flex", justifyContent: "flex-end" }}>
+      <p
+        onClick={() => onRouteChange("signin")}
+        className="f3 link dim black underline pa3 pointer"
+      >
+        Sign In
+      </p>
+      <p
+        onClick={() => onRouteChange("register")}
+        className="f3 link dim black underline pa3 pointer"
+      >
+        Register
+      </p>
+    </div>
+  );
+};
 
 export default Navigation;
