0c3533801ecd5c3664a14610fd84d3b5616f4b58
Sadia Waheed <sadia.waheed312@hotmail.com>
2023-05-31 16:18:35 +0300

    Fixing aesthetics of Register.js

--- src/components/Register/Register.js
+++ src/components/Register/Register.js
@@ -1,8 +1,8 @@
-import React from 'react';
+import React from "react";
 
-const Register = ({onRouteChange}) =>{
-    return(
-        <article className="br3 ba b--black-10 mv4 w-100 w-50-m w-25-l mw6 shadow-5 center">
+const Register = ({ onRouteChange }) => {
+  return (
+    <article className="br3 ba b--black-10 mv4 w-100 w-50-m w-25-l mw6 shadow-5 center">
       <main className="pa4 black-80">
         <div className="measure">
           <fieldset id="sign_up" className="ba b--transparent ph0 mh0">
@@ -43,7 +43,7 @@ const Register = ({onRouteChange}) =>{
           </fieldset>
           <div className="">
             <input
-              onClick={() => onRouteChange('home')} 
+              onClick={() => onRouteChange("home")}
               className="b ph3 pv2 input-reset ba b--black bg-transparent grow pointer f6 dib"
               type="submit"
               value="Register"
@@ -52,7 +52,7 @@ const Register = ({onRouteChange}) =>{
         </div>
       </main>
     </article>
-    )
-}
+  );
+};
 
 export default Register;
