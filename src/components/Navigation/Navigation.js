909a344d99240ee2555dc14400c6eadf4f7ae784
Sadia Waheed <sadia.waheed312@hotmail.com>
2023-06-07 11:33:05 +0300

    fixes in navidgation

--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -1,31 +1,35 @@
 import React from "react";
 
 const Navigation = ({ onRouteChange, isSignedIn }) => {
-  return isSignedIn ? (
-    <div style={{ display: "flex", justifyContent: "flex-end" }}>
-      <p
-        onClick={() => onRouteChange("signout")}
-        className="f3 link dim black underline pa3 pointer"
-      >
-        Sign Out
-      </p>
-    </div>
-  ) : (
-    <div style={{ display: "flex", justifyContent: "flex-end" }}>
-      <p
-        onClick={() => onRouteChange("signin")}
-        className="f3 link dim black underline pa3 pointer"
-      >
-        Sign In
-      </p>
-      <p
-        onClick={() => onRouteChange("register")}
-        className="f3 link dim black underline pa3 pointer"
-      >
-        Register
-      </p>
-    </div>
-  );
+  if (isSignedIn) {
+    return (
+      <nav style={{ display: "flex", justifyContent: "flex-end" }}>
+        <p
+          onClick={() => onRouteChange("signout")}
+          className="f3 link dim black underline pa3 pointer"
+        >
+          Sign Out
+        </p>
+      </nav>
+    );
+  } else {
+    return (
+      <nav style={{ display: "flex", justifyContent: "flex-end" }}>
+        <p
+          onClick={() => onRouteChange("signin")}
+          className="f3 link dim black underline pa3 pointer"
+        >
+          Sign In
+        </p>
+        <p
+          onClick={() => onRouteChange("register")}
+          className="f3 link dim black underline pa3 pointer"
+        >
+          Register
+        </p>
+      </nav>
+    );
+  }
 };
 
 export default Navigation;
