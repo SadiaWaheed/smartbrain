392902ef0e753555222fc8bb33f840d990a418e1
Sadia Waheed <sadia.waheed312@hotmail.com>
2023-06-07 16:46:25 +0300

    deployemnt links updated

--- src/App.js
+++ src/App.js
@@ -67,7 +67,7 @@ class App extends Component {
   onButtonSubmit = () => {
     this.setState({ imageUrl: this.state.input });
 
-    fetch("http://localhost:3000/imageurl", {
+    fetch("https://samrt-brain-api.onrender.com/imageurl", {
       method: "post",
       headers: { "Content-Type": "application/json" },
       body: JSON.stringify({
@@ -77,7 +77,7 @@ class App extends Component {
       .then((response) => response.json())
       .then((response) => {
         if (response) {
-          fetch("http://localhost:3000/image", {
+          fetch("https://samrt-brain-api.onrender.com/image", {
             method: "put",
             headers: { "Content-Type": "application/json" },
             body: JSON.stringify({
